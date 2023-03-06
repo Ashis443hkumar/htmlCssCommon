1de9d3a0f3a349a5bc31a073de5adb36c877defe
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-03-06 19:55:07 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello!');
+
+const optionMenu = document.querySelector(".select-menu"),
+      optionBtn = optionMenu.querySelector(".select-btn"),
+      options = optionMenu.querySelectorAll(".option"),
+      Sbtn_text = optionMenu.querySelector(".Sbtn-text");
+
+      selectBtn = addEventListener("click", () => optionMenu.classList.toggle("active"));
+      
+     options.forEach( option =>{
+        option.addEventListener("click" , ()=>{
+          let selectOption = option.querySelector(".option_text").innerHTML;
+          Sbtn_text.innerHTML = selectOption
+          console.log(option)
+        })
+      })
+
+ 
